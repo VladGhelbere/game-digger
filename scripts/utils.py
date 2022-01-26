9a7eed5cdd2c083ec8ddc1e9c071559ef01ffd41
VladGhelbere <vladut.ghelbere98@e-uvt.ro>
2022-01-26 19:14:16 +0200

    utils - added db Game class to utils, deleted almost all old functions

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,30 +1,18 @@
-def get_random_game_str(game_obj, requester):
-    c_string = f"Hey {requester}, here's a random game: '{game_obj.name}'\n"
+class Game:
+    site_url = "https://rawg.io/"
+    def __init__(self,name,slug,platforms,genres,stores,released):
+        self.name = name
+        self.slug = slug
+        self.url = self.site_url+'games/'+self.slug
+        self.platforms = platforms
+        self.genres = genres
+        self.stores = stores
+        self.released = released
 
-    if game_obj.genres != []:
-        c_string += f"Genres: {', '.join(game_obj.genres)}\n"
-    if game_obj.platforms != []:
-        c_string += f"Platforms: {', '.join(game_obj.platforms)}\n"
-    if game_obj.release_date != None:
-        c_string += f"Released date: {game_obj.release_date}\n"
-    if game_obj.url != None:
-        c_string += f"More info: {game_obj.url}"
-
-    return c_string
-
-def get_rec_game_str(game_obj, requester):
-    c_string = f"My recommendation for {requester} is: '{game_obj.name}'\n"
-
-    if game_obj.genres != []:
-        c_string += f"Genres: {', '.join(game_obj.genres)}\n"
-    if game_obj.platforms != []:
-        c_string += f"Platforms: {', '.join(game_obj.platforms)}\n"
-    if game_obj.release_date != None:
-        c_string += f"Released date: {game_obj.release_date}\n"
-    if game_obj.url != None:
-        c_string += f"More info: {game_obj.url}"
-
-    return c_string
+    def get_game_as_rec(self, requester):
+        c_string = f"My recommendation for {requester} is: '{self.name}'\n"
+        c_string += f"Genres: {self.genres}\nPlatforms: {self.platforms}\nStores: {self.stores}\nReleased date: {self.released}\nMore info: {self.url}"
+        return c_string
 
 def get_req_game_str(game_obj, requester, mention=None):
     c_string = f"Game: '{game_obj.name}'\n\n"
@@ -32,26 +20,18 @@ def get_req_game_str(game_obj, requester, mention=None):
         c_string += f"Hey {mention}, {requester} wants you to check out this game !\n"
     else:
         c_string += f"Requested by {requester}\n"
-
     if game_obj.genres != []:
         c_string += f"Genres: {', '.join(game_obj.genres)}\n"
-
     if game_obj.platforms != []:
         c_string += f"Platforms: {', '.join(game_obj.platforms)}\n"
-
     if game_obj.release_date != None:
         c_string += f"Released date: {game_obj.release_date}\n"
-
     if game_obj.user_rating != 0:
         c_string += f"User Rating: {game_obj.user_rating}/5\n"
-
     if game_obj.metacritic != None:
         c_string += f"Metacritic: {game_obj.metacritic}/100\n"
-
     if game_obj.stores != []:
         c_string += f"Stores: {', '.join(game_obj.stores)}\n"
-
     if game_obj.url != None:
         c_string += "More info: "+game_obj.url
-
     return c_string
